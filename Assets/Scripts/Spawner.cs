26d303e5d148c38439f5eef28cfc1c72f7fe6a2a
5urviliuk5 <145654904+5urviliuk5@users.noreply.github.com>
2024-03-03 17:35:51 +0200

    spawning

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,19 +6,34 @@ public class Spawner : MonoBehaviour
 {
     public GameObject prefab;
     public List<Transform> spawnPoints;
+    public List<int> enemiesPerWave;
+
+    [Range(0.1f, 10f)]
+    public float spawnInterval = 1f;
+    [Range(0.1f, 10f)]
+    public float timeBetweenWaves = 5f;
+    public int enemiesToSpawn;
 
     async void Start()
     {
-        await new WaitForSeconds(2f);
-        Spawn();
+        foreach (var num in enemiesPerWave)
+        {
+            enemiesToSpawn = num;
+
+            while (enemiesToSpawn > 0)
+            {
+                await new WaitForSeconds(spawnInterval);
+                Spawn();
+                enemiesToSpawn--;
+            }
+
+            await new WaitForSeconds(timeBetweenWaves);
+        }
     }
 
     void Spawn()
     {
-        while (true)
-        {
-            var point = spawnPoints[Random.Range(0, spawnPoints.Count)];
-            Instantiate(prefab, point.position, point.rotation);
-        }
+        var point = spawnPoints[Random.Range(0, spawnPoints.Count)];
+        Instantiate(prefab, point.position, point.rotation);
     }
 }
